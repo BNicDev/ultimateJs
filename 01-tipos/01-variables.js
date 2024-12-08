c3ee86133ef6e06a7d72f3b17bb8eb9ac7f3e0e3
BNicDev <bnicdev@gmail.com>
2024-12-08 11:31:47 -0300

    types of var

--- 01-tipos/01-variables.js
+++ 01-tipos/01-variables.js
@@ -1,3 +1,8 @@
+//UpperCamelCase, camelcase, snake_case
 let nombre = "Hola Mundo";
+let NombreCompleto; //UpperCamelCase
+let nombreCompleto; //camelCase
+let nombre_completo; //snake_case
 
 console.log(nombre);
+let apellido, animal; // se puede pero es de buena practica inicializarla en dos lineas distintas
