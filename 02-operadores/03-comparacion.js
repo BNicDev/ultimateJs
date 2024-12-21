f9ca84d4746b10e1f4b17ca4b3e5b61765e4afa7
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2024-12-21 11:26:04 -0300

    unit 2

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,16 @@
+//comparadores relacionales
+
+let a = 10; 
+console.log(a>5);
+console.log(a>=5);
+console.log(a<5);
+console.log(a<=5);
+
+// comparadores de igualdad
+
+console.log(a==10);
+console.log(a!=10);
+console.log(a=='10');
+console.log(a==='10');
+
+
