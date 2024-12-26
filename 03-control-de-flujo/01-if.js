a6cbfeca9683628ba00854b787e1e4f56530f85b
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2024-12-26 10:56:33 -0300

    unit 3

---
+++ 03-control-de-flujo/01-if.js
@@ -0,0 +1,10 @@
+let edad = 16;
+
+if(edad>17){
+    console.log("Eres mayor de edad");
+}else if(edad>13){
+    console.log("necesita estar en compania de un adulto");
+}
+else{
+    console.log("Eres menor de edad");
+}
