8b2f38faab9d1f71992c2524a44e8a690b8142ab
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2025-01-24 15:27:57 -0300

    ejercicios

---
+++ 06-ejercicios2/05.js
@@ -0,0 +1,12 @@
+let objeto = {
+    id: 1,
+    name: 'chanchito',
+    login : function(){},
+    logout: function(){},
+}
+let propiedad = 'name';
+
+function tieneProp(obj, propiedad){};
+
+
+console.log(tieneProp(objeto,propiedad))
