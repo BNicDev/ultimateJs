8abe8184f7ddbac90dfbd492a8c35d8116da0642
nicolas <bnicde@gmail.com>
2025-01-30 11:16:29 -0300

    arrays

---
+++ 07-arrays/09-join.js
@@ -0,0 +1,8 @@
+let arr1 = ['Chanchito', 'Feliz', 'Felipe'];
+
+let mensaje = arr1.join(', ');//los une con una coma y un espacio
+// console.log(mensaje); // Chanchito, Feliz, Felipe
+
+let saludo = 'Hola mundo! desde ARG';
+let dividido = saludo.split(' ');//divide el string en un array
+console.log(dividido.join('-')); // ['Hola', 'mundo!', 'desde', 'ARG']
