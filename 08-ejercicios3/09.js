db754cc29c980a1946de850e665f07a63d49b695
nicolas <bnicde@gmail.com>
2025-02-08 22:40:24 -0300

    ejercicios

--- 08-ejercicios3/09.js
+++ 08-ejercicios3/09.js
@@ -11,8 +11,35 @@ const users = [
 ]
 
 //unificar las estructuras de usuarios y user 
+function unificarEstructuras(usrs,usrs2) {
+    const usuariosUnificados = usrs.map(usuario => {
+        return{
+            age: usuario.edad,
+            name: usuario.nombre,
+            membership: usuario.plan
+        }
+    })
+    let unidos = unificados(usuariosUnificados, usrs2);
+    let ordenados = ordenarEdad(unidos)
+    return obtenerDatos(ordenados);
+}
+
 //fusionar los arrays
+function unificados(usrs, usrs2) {
+     return usrs.concat(usrs2); 
+}
 //ordenar por edad
+function ordenarEdad(usrs){
+    let ordenados = usrs.sort((a,b) => a.age - b.age);
+    return ordenados;
+}
 //crear plantilla HTML:
+function obtenerDatos(usrs){
+    let datos = usrs.map(el => `<li>nombre: ${el.name}, age: ${el.age}</li>`)
+    let joineados = `<ul>${datos.join('')}</ul>`
+    return joineados
+};
 //<li>nombre: name, edad: age</li>
 //imprimir la lista en consola
+
+console.log(unificarEstructuras(usuarios, users));
