6ad2e9614ea3f1bc3ca70fee5afab89c75c63f95
nicolas <bnicde@gmail.com>
2025-02-26 22:07:19 -0300

    dom

---
+++ 13-dom/01-selectores.js
@@ -0,0 +1,28 @@
+//HTMLElement
+let htmlElement = document.getElementById('cuerpo');
+
+//HTMLCollection => se parece a un array pero no lo es
+let elementosred = document.getElementsByClassName('red');
+
+
+//nodeList 
+let elementosChanchito = document.getElementsName('chanchito');
+
+//HTMLCollection
+let parrafos = document.getElementsByTagName('p');
+
+
+//HTMLElement <= solo un elemento
+let el = document.querySelector('#cuerpo');
+
+
+//nodeList <= varios elementos
+let els = document.querySelectorAll('p');
+
+let plive = document.getElementsByTagName('p');
+let pstatic = document.querySelectorAll('p');
+
+let nuevop = document.createElement('p');
+document.body.append(nuevop);
+
+console.log(plive,pstatic)
